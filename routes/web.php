7e18bba6ce46cf4b4de5f0bb84add4af153ce0c9
Alex Esaki <apachecordovax@gmail.com>
2024-07-27 18:50:08 +0200

    show threads via a tag, route and create scopeForTag

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Pages\TagController;
 use App\Http\Controllers\Pages\ThreadController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
@@ -30,6 +31,10 @@ Route::group(['prefix' => 'threads', 'as' => 'threads.'], function () {
     Route::get('create', [ThreadController::class, 'create'])->name('create');
     Route::post('/', [ThreadController::class, 'store'])->name('store');
     Route::get('/{category:slug}/{thread:slug}', [ThreadController::class, 'show'])->name('show');
+
+    Route::group(['as' => 'tags.'], function () {
+        Route::get('/{tag:slug}', [TagController::class, 'index'])->name('index');
+    });
 });
 
 Route::get('dashboard/users', [PageController::class, 'users'])->name('users');
