bdc962fd58ec2ac712a1605e993cba0c61f59080
Alex Esaki <apachecordovax@gmail.com>
2024-07-30 10:11:22 +0200

    update the indicator and count livewire classes

--- app/Http/Livewire/Notifications/Count.php
+++ app/Http/Livewire/Notifications/Count.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\Notifications;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Count extends Component
 {
-    public function render()
+    public $count;
+
+    public function render(): View
     {
-        return view('livewire.notifications.count');
+        $this->count = Auth::user()->unreadNotifications()->count();
+
+        return view('livewire.notifications.count', [
+            'count' => $this->count,
+        ]);
     }
 }
--- app/Http/Livewire/Notifications/Indicator.php
+++ app/Http/Livewire/Notifications/Indicator.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire\Notifications;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Indicator extends Component
 {
-    public function render()
+    public $hasNotifications;
+
+    public function render(): View
     {
-        return view('livewire.notifications.indicator');
+        $this->hasNotifications = $this->setHasNotifications(Auth::user()->unreadNotifications()->count());
+
+        return view('livewire.notifications.indicator', [
+            'hasNotifications' => $this->hasNotifications,
+        ]);
+    }
+
+    public function setHasNotifications(int $count): bool
+    {
+        return $count > 0;
     }
 }
